2fcbefa16f6b877d70b636a57e7fc40c691148e2
Mathew Uy <43997633+JinzUtAreZ@users.noreply.github.com>
2020-02-24 18:08:28 +0800

    From office
    may error pa ung viewing

--- App_Code/UploadService.cs
+++ App_Code/UploadService.cs
@@ -1,7 +1,13 @@
-﻿using System;
+using System;
 using System.Web;
 using System.Web.Services;
 using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Script.Serialization;
+using System.ComponentModel;
+using System.Drawing;
+using System.Collections.Generic;
 
 /// <summary>
 /// Summary description for WebServiceCS
@@ -9,7 +15,7 @@ using System.IO;
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-// [System.Web.Script.Services.ScriptService]
+[System.Web.Script.Services.ScriptService]
 public class UploadService : System.Web.Services.WebService {
 
     public UploadService()
@@ -18,11 +24,111 @@ public class UploadService : System.Web.Services.WebService {
         //InitializeComponent(); 
     }
 
+    public string Connect()
+    {
+        return System.Configuration.ConfigurationManager.ConnectionStrings["EQMConnection"].ToString();
+    }
+
     [WebMethod]
     public void UploadFile(string fileName, string contentType, byte[] bytes)
     {
         //Save the Byte Array as File.
-        string filePath = Server.MapPath(string.Format("~/Uploads/{0}", fileName));
-        File.WriteAllBytes(filePath, bytes);
+        //string filePath = Server.MapPath(string.Format("~/Uploads/{0}", fileName));
+        //File.WriteAllBytes(filePath, bytes);
+        
+        string errmsg = "" ;
+        using (SqlConnection sqlcon = new SqlConnection(Connect()))
+        {
+            sqlcon.Open();
+
+            try
+            {
+                SqlCommand sqlcmd = new SqlCommand("spSave_ImageData", sqlcon);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                sqlcmd.Parameters.Add("@inFileName", SqlDbType.VarChar).Value = fileName;
+                sqlcmd.Parameters.Add("@inType", SqlDbType.VarChar).Value = contentType;
+                sqlcmd.Parameters.Add("@inData", SqlDbType.Image, bytes.Length).Value = bytes;
+                SqlDataReader sqlrd = sqlcmd.ExecuteReader();
+
+                if (sqlrd.HasRows)
+                {
+                    while (sqlrd.Read())
+                    {
+                        errmsg = "Error in loading Service Deploy List";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errmsg = "Error in loading Service Deploy List";
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+
+        }
+    }
+
+    public class FileData
+    { 
+        public string FileName;
+        public string ContentType;
+        public string ImageData;
+    }
+
+    [WebMethod]
+    public string ViewFile()
+    {
+        List<FileData> newdt = new List<FileData>();
+        string errmsg = "";
+        using (SqlConnection sqlcon = new SqlConnection(Connect()))
+        {
+            sqlcon.Open();
+
+            try
+            {
+                SqlCommand sqlcmd = new SqlCommand("spLoad_ImageData", sqlcon);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                
+                SqlDataReader sqlrd = sqlcmd.ExecuteReader();
+
+                if (sqlrd.HasRows)
+                {
+                    
+                    while (sqlrd.Read())
+                    {
+                       
+                        //Get the byte array from image file
+                        //FileName = sqlrd["Base64Data"];
+                        byte[] imgBytes = (byte[])sqlrd["Data"];
+
+                        //If you want convert to a bitmap file
+                        TypeConverter tc = TypeDescriptor.GetConverter(typeof(Bitmap));
+                        Bitmap MyBitmap = (Bitmap)tc.ConvertFrom(imgBytes);
+
+                        //string imgString = Convert.ToBase64String(imgBytes);
+                        newdt.Add(new FileData {
+                            FileName = sqlrd["ImageName"].ToString(), 
+                            ContentType = sqlrd["ContentType"].ToString(),
+                            ImageData = Convert.ToBase64String(imgBytes),
+                        });
+                        //Set the source with data:image/bmp
+                        //img.Src = String.Format("data:image/Bmp;base64,{0}\"", imgString);   //img is the Image control ID
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errmsg = "Error in loading Service Deploy List";
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+
+        }
+
+        return newdt;
     }
 }
